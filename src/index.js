bda73c8b295ba6441968aa4ab137ffe8d3c8ba1f
meerul <meerul@someone.co>
2020-09-02 12:52:24 +0800

    Moved toggle to utils

--- src/index.js
+++ src/index.js
@@ -13,32 +13,7 @@ const classs = new Kelas();
 classs.setQs(user.qs);
 classs.getClass(user.qs);
 
+const util = new Util();
 
 
-let classSelectChanged = document.getElementById("classList");
-classSelectChanged.addEventListener("change", () => window.open("firebase.html" + "?class=" + document.getElementById("classList").childNodes[1].value, "_self"));
-
-let addStudentModal = document.getElementById("addStudentToggler"), 
-bgStudentModal = document.getElementById("bgStudentModal"), 
-ariaCloseStudentModal = document.getElementById("ariaCloseStudentModal"), 
-cancelStudentModal = document.getElementById("cancelStudentModal");
-
-addStudentModal.addEventListener("click", () =>  document.querySelector("#newStudent").classList.add("is-active") );
-
-bgStudentModal.addEventListener("click", () => { 
-      document.getElementsByClassName("input");
-      document.getElementsByName("classList4")[0].value = "";
-      document.querySelector("#newStudent").classList.remove("is-active");
-});
-ariaCloseStudentModal.addEventListener("click", () => { 
-      document.getElementsByClassName("input");
-      document.getElementsByName("classList4")[0].value = "";
-      document.querySelector("#newStudent").classList.remove("is-active");
-});
-
-cancelStudentModal.addEventListener("click", () => { 
-      document.getElementsByClassName("input");
-      document.getElementsByName("classList4")[0].value = "";
-      document.querySelector("#newStudent").classList.remove("is-active");
-});
 //setAttribute("onchange", this.getSelectedClass());
--- utils/util.js
+++ utils/util.js
@@ -1,5 +1,31 @@
 class Util {
       constructor() {
+            let classSelectChanged = document.getElementById("classList");
+            classSelectChanged.addEventListener("change", () => window.open("firebase.html" + "?class=" + document.getElementById("classList").childNodes[1].value, "_self"));
+
+            let addStudentModal = document.getElementById("addStudentToggler"),
+                  bgStudentModal = document.getElementById("bgStudentModal"),
+                  ariaCloseStudentModal = document.getElementById("ariaCloseStudentModal"),
+                  cancelStudentModal = document.getElementById("cancelStudentModal");
+
+            addStudentModal.addEventListener("click", () => document.querySelector("#newStudent").classList.add("is-active"));
+
+            bgStudentModal.addEventListener("click", () => {
+                  document.getElementsByClassName("input");
+                  document.getElementsByName("classList4")[0].value = "";
+                  document.querySelector("#newStudent").classList.remove("is-active");
+            });
+            ariaCloseStudentModal.addEventListener("click", () => {
+                  document.getElementsByClassName("input");
+                  document.getElementsByName("classList4")[0].value = "";
+                  document.querySelector("#newStudent").classList.remove("is-active");
+            });
+
+            cancelStudentModal.addEventListener("click", () => {
+                  document.getElementsByClassName("input");
+                  document.getElementsByName("classList4")[0].value = "";
+                  document.querySelector("#newStudent").classList.remove("is-active");
+            });
             // this.sel = document.createElement("select");
             // this.sel2 = document.createElement("select");
             // this.attandanceList = [];
@@ -15,7 +41,7 @@ class Util {
                   document.querySelector("#classList").appendChild(sel);
                   document.querySelector("#newStudent").classList.remove("is-active");
             }
-      
+
       }
       submitForm(e) {
             e.preventDefault();
@@ -24,7 +50,7 @@ class Util {
             let name = formData.get('name');
             let kelas = parseInt(formData.get('classList4'));
             insertUser(name, kelas)
-      
+
       }
       selectClass(val) {
             window.open("firebase.html" + "?class=" + document.getElementById("classList").childNodes[1].value, "_self")
@@ -41,7 +67,7 @@ class Util {
       }
 }
 export { Util as default };
-// kelas.where("class_id","==",6).orderBy("class_name").get().then((querySnapshot) => {   
+// kelas.where("class_id","==",6).orderBy("class_name").get().then((querySnapshot) => {
 //       querySnapshot.forEach((doc) => {
 //             console.log(doc.data().class_name)
 //       })})
